a96184e77447ef7fe756016c2016d0dcd9f2fd34
islamdiab-stack <islamdiab967@gmail.com>
2022-07-01 15:18:38 +0300

    create a new migration to article

---
+++ database/migrations/2022_07_01_090659_create_atricles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAtriclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atricles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->text('full_text');
+            $table->string('image')->nullable();
+
+            $table->integer('tag_id')->unsigned();
+
+            $table->timestamps();
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atricles');
+    }
+}
