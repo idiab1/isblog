817caa81faff2b2f4445bc71bdbc1ad9e100e72a
islamdiab-stack <islamdiab967@gmail.com>
2022-07-01 15:15:06 +0300

    add all column in fillable array and create relationship between article, tag where all tags belongs to article

---
+++ app/Models/Tag.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+    protected $fillable = ["name"];
+
+    /**
+     * Get the article that owns the Tag
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
