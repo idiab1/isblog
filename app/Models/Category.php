0590320546366abe6ed1a104563f826fac2609c5
islamdiab-stack <islamdiab967@gmail.com>
2022-07-01 15:17:18 +0300

    add all column in fillable array and create relationship between article, category where all category belongs to article

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["name"];
+
+    /**
+     * Get the article that owns the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
