e10af75e78d0e6791346265be030b78b82597fc7
islamdiab-stack <islamdiab967@gmail.com>
2022-07-02 07:43:13 +0300

    create the layout for homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,60 +1,67 @@
 @extends('layouts.app')
 
+{{-- Header Info --}}
+@section('header-info')
+    <h1>{{App\Models\Setting::first()->name}}</h1>
+    <!-- Social Links -->
+    <ul class="list-unstyled">
+        <li>
+            <!-- Facebook -->
+            <a class="" href="#">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+        </li>
+        <li>
+            <!-- Github -->
+            <a class="" href="#">
+                <i class="fab fa-github"></i>
+            </a>
+        </li>
+        <li>
+            <!-- Linkin -->
+            <a class="" href="#">
+                <i class="fab fa-linkedin"></i>
+            </a>
+        </li>
+        <li>
+            <!-- twitter -->
+            <a class="" href="#">
+                <i class="fab fa-twitter"></i>
+            </a>
+        </li>
+    </ul>
+@endsection
+
 {{-- Content --}}
 @section('content')
 <!-- Homepage -->
 <section class="homepage section">
     <!-- Container -->
     <div class="container">
-        <!-- Header -->
-        <header class="header">
-            <div class="row">
-                <div class="col-6 mx-auto">
-                    <!-- Header Info -->
-                    <div class="header-info">
-                        <!-- User Image -->
-                        <div class="user-image">
-                            <img class="img-fluid" src="" alt="">
-                        </div>
-                        <!-- User Info -->
-                        <div class="user-info">
-                            <h1>User name</h1>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                Iusto ex enim voluptas, cupiditate animi corporis!</p>
-                            <!-- Social Links -->
-                            <ul class="list-unstyled">
-                                <li>
-                                    <!-- Facebook -->
-                                    <a class="" href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <!-- Github -->
-                                    <a class="" href="#">
-                                        <i class="fab fa-github"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <!-- Linkin -->
-                                    <a class="" href="#">
-                                        <i class="fab fa-linkedin"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <!-- twitter -->
-                                    <a class="" href="#">
-                                        <i class="fab fa-twitter"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- ./header-info -->
-                </div>
-            </div>
-        </header>
-        <!-- ./header -->
+        <!-- Some Tags -->
+        <div class="some-tags">
+            <ul class="list-unstyled">
+                <li>
+                    <a href="#">#tag</a>
+                </li>
+                <li>
+                    <a href="#">#tag</a>
+                </li>
+                <li>
+                    <a href="#">#tag</a>
+                </li>
+                <li>
+                    <a href="#">#tag</a>
+                </li>
+                <li>
+                    <a href="#">#tag</a>
+                </li>
+                <li>
+                    <a href="#">#tag</a>
+                </li>
+            </ul>
+        </div>
+        <!-- ./some-tags -->
 
         <!-- Articles Content -->
         <div class="articles-content">
@@ -70,25 +77,33 @@
                         <div class="card-body">
                             <!-- Tags -->
                             <ul class="list-unstyled">
-                                <li>Tag</li>
-                                <li>Tag</li>
-                                <li>Tag</li>
-                                <li>Tag</li>
+                                <li>
+                                    <a href="#">#tag</a>
+                                </li>
+                                <li>
+                                    <a href="#">#tag</a>
+                                </li>
+                                <li>
+                                    <a href="#">#tag</a>
+                                </li>
+                                <li>
+                                    <a href="#">#tag</a>
+                                </li>
                             </ul>
                             <!-- ./tags -->
                             <h2>Article Title</h2>
-
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, atque.</p>
                         </div>
                         <!-- ./card-body -->
                         <!-- Card Footer -->
                         <div class="card-footer">
                             <!-- Image -->
                             <div class="image">
-                                <img src="{{asset("uploads/default.png")}}" alt="User avatar" width="30px">
+                                <img class="img-fluid" src="{{asset("uploads/default.png")}}" alt="User avatar">
                             </div>
                             <!-- Card Footer -->
                             <div class="user-info">
-                                <h3>User name</h3>
+                                <a href="#">User name</a>
                             </div>
                         </div>
                         <!-- ./card-footer -->
