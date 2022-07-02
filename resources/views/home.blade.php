e78a9fb75726469836de0c254f64647824fc3040
islamdiab-stack <islamdiab967@gmail.com>
2022-07-02 07:51:58 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,7 +66,7 @@
         <!-- Articles Content -->
         <div class="articles-content">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-12 col-md-4">
                     <!-- card -->
                     <div class="card card-article">
                         <!-- Card Header -->
