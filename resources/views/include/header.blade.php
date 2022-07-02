88f1286bc4c5f40a0f265279e8b0376c35423879
islamdiab-stack <islamdiab967@gmail.com>
2022-07-02 07:02:04 +0300

    create a new header component

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,60 @@
+<!-- Header -->
+<header class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Header info -->
+                <div class="header-info text-center">
+                    @yield('header-info')
+                </div>
+                <!-- ./header-info -->
+            </div>
+        </div>
+    </div>
+    {{-- <div class="row">
+        <div class="col-6 mx-auto">
+            <!-- Header Info -->
+            <div class="header-info">
+                <!-- User Image -->
+                <div class="user-image">
+                    <img class="img-fluid" src="" alt="">
+                </div>
+                <!-- User Info -->
+                <div class="user-info">
+                    <h1>User name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Iusto ex enim voluptas, cupiditate animi corporis!</p>
+                    <!-- Social Links -->
+                    <ul class="list-unstyled">
+                        <li>
+                            <!-- Facebook -->
+                            <a class="" href="#">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- Github -->
+                            <a class="" href="#">
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- Linkin -->
+                            <a class="" href="#">
+                                <i class="fab fa-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- twitter -->
+                            <a class="" href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- ./header-info -->
+        </div>
+    </div> --}}
+</header>
+<!-- ./header -->
