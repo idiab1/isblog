6ea8b9e69e333f179d9613dba75a41fdc5f5d810
islamdiab-stack <islamdiab967@gmail.com>
2022-07-03 11:54:46 +0300

    create a new component to footer

---
+++ resources/views/include/dashboard/footer.blade.php
@@ -0,0 +1,17 @@
+<!-- Footer -->
+<footer class="footer pt-3  ">
+    <!-- Container fluid -->
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <!-- Copyright -->
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    <p class="m-0">&copy; Copyright all reserved.</p>
+                </div>
+                <!-- ./copyright -->
+            </div>
+        </div>
+    </div>
+    <!-- ./container-fluid -->
+</footer>
+<!-- ./footer -->
