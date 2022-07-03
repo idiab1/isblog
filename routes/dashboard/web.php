cf0937eb7fba14ebf9fd0af42000c393fbeb105f
islamdiab-stack <islamdiab967@gmail.com>
2022-07-03 11:45:16 +0300

    create a new route to admin home with get type

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix("Dashboard")->middleware(['auth', 'is_admin'])->group(function () {
 
     // -->>> Admin Home Route
-    Route::get('/', function () {
-        return "Admin Home";
-    })->name("admin.home");
+    Route::get('/home', [HomeController::class, "index"])->name("admin.home");
+
 });
