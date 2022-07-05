fca0900b982c5d4c9cb123f627281da5c34549fa
islamdiab-stack <islamdiab967@gmail.com>
2022-07-05 18:25:24 +0300

    change container to container fluid

--- resources/views/include/dashboard/footer.blade.php
+++ resources/views/include/dashboard/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="footer pt-3  ">
     <!-- Container fluid -->
-    <div class="container-fluid">
+    <div class="container">
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <!-- Copyright -->
