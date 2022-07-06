c68a74e77634147c19ff8e1f061dc6484bed0a76
islamdiab-stack <islamdiab967@gmail.com>
2022-07-06 17:02:43 +0300

    import category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             TagSeeder::class,
+            CategorySeeder::class,
             SettingSeeder::class
         ]);
     }
