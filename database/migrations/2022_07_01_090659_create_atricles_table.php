d2b770080ee7a066ecaf0fce877368c1f1a4fb4a
islamdiab-stack <islamdiab967@gmail.com>
2022-07-06 11:15:01 +0300

    change name of articles table

--- database/migrations/2022_07_01_090659_create_atricles_table.php
+++ database/migrations/2022_07_01_090659_create_atricles_table.php
@@ -13,7 +13,7 @@ class CreateAtriclesTable extends Migration
      */
     public function up()
     {
-        Schema::create('atricles', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 100);
             $table->text('full_text');
