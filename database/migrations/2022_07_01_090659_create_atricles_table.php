974dcb7a2018020a25089c9c26e7affe6701fad8
islamdiab-stack <islamdiab967@gmail.com>
2022-07-06 16:59:33 +0300

    remove tag_id column

--- database/migrations/2022_07_01_090659_create_atricles_table.php
+++ database/migrations/2022_07_01_090659_create_atricles_table.php
@@ -18,11 +18,7 @@ class CreateAtriclesTable extends Migration
             $table->string('name', 100);
             $table->text('full_text');
             $table->string('image')->nullable();
-
-            $table->integer('tag_id')->unsigned();
-
             $table->timestamps();
-            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
 
