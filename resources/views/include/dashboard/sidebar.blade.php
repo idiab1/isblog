26d159d61b80249600875bf0341fddaa524a160a
islamdiab-stack <islamdiab967@gmail.com>
2022-07-11 20:49:43 +0300

    add name of route to admin homepage

--- resources/views/include/dashboard/sidebar.blade.php
+++ resources/views/include/dashboard/sidebar.blade.php
@@ -6,7 +6,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute
             end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="#" target="_blank">
+        <a class="navbar-brand m-0" href="{{route("admin.home")}}" target="_blank">
             <span class="ms-1 font-weight-bold">{{App\Models\Setting::first()->name}}</span>
         </a>
     </div>
@@ -17,7 +17,7 @@
         <ul class="navbar-nav">
             <!-- Dashboard -->
             <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link active" href="{{route("admin.home")}}">
                     <!-- Icon -->
                     <div class="icon icon-shape icon-sm border-radius-md text-center
                         me-2 d-flex align-items-center justify-content-center">
