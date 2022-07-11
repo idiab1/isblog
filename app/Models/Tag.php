d80ff68f20c07c72d8d029c806120d3fb2cadc6c
islamdiab-stack <islamdiab967@gmail.com>
2022-07-11 14:42:34 +0300

    change name of method to tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -15,7 +15,7 @@ class Tag extends Model
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function article()
+    public function articles()
     {
         return $this->belongsToMany(Article::class);
     }
