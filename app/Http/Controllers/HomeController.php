9def88a365536431b24e34579662d29696b2177d
islamdiab-stack <islamdiab967@gmail.com>
2022-07-13 18:23:33 +0300

    get sex records from articles table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Get all articles
+        $articles = Article::with("tags")->orderBy('created_at', 'desc')
+            ->take(6)
+            ->get();
+        return view('home', compact("articles"));
     }
 }
