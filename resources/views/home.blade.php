a7167a6ce91763ff9128428d92fcd66282f4ba7f
islamdiab-stack <islamdiab967@gmail.com>
2022-07-14 22:29:38 +0300

    add name of route to access on articles related it

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,7 @@
                                         @if ($article->tags()->count() > 0)
                                             @foreach ($article->tags as $tag)
                                                 <li>
-                                                    <a href="{{route("tags.show", ["id" => $tag->id])}}">{{"#".$tag->name}}</a>
+                                                    <a href="{{route("front.tags.show", ["id" => $tag->id])}}">{{"#".$tag->name}}</a>
                                                 </li>
                                             @endforeach
                                         @endif
