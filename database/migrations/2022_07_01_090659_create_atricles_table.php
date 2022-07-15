a5ee99aa88cda683cb9e87df4be692d65c16b2ac
islamdiab-stack <islamdiab967@gmail.com>
2022-07-15 20:41:23 +0300

    create a new column to user_id

--- database/migrations/2022_07_01_090659_create_atricles_table.php
+++ database/migrations/2022_07_01_090659_create_atricles_table.php
@@ -20,9 +20,11 @@ class CreateAtriclesTable extends Migration
             $table->string('image')->nullable();
 
             $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
 
             $table->timestamps();
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
