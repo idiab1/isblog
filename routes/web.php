129f8d8a63351c9d4301400025f36da1e72a0b4a
islamdiab-stack <islamdiab967@gmail.com>
2022-07-16 02:59:36 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
