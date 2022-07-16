e741131648bfd5ff33f3a4551d914bcc040c1bd2
islamdiab-stack <islamdiab967@gmail.com>
2022-07-16 05:59:43 +0300

    write update method to update data of users

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class ProfileController extends Controller
 {
@@ -68,7 +72,50 @@ class ProfileController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // dd($request);
+        // Get user by id
+        $user = User::find($id);
+        $request_date = $request->all();
+
+        if($request->has("image")){
+            if($user->image){
+                Storage::disk("public_uploads")->delete('/users/' . $user->image);
+            }
+
+            Image::make($request->image)->resize(300, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save(public_path('uploads/users/' . $request->image->hashName()));
+
+            $request_date["image"] = $request->image->hashName();
+
+            $user->update([
+                "image"         => $request->image->hashName(),
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => Hash::make($request->password),
+                "linkedin_url" => $request->linkedin_url,
+                "github_url" => $request->github_url,
+                "facebook_url" => $request->facebook_url,
+                "twitter_url" => $request->twitter_url,
+            ]);
+
+            return redirect()->back();
+
+        }else{
+            $user->update([
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => Hash::make($request->password),
+                "linkedin_url" => $request->linkedin_url,
+                "github_url" => $request->github_url,
+                "facebook_url" => $request->facebook_url,
+                "twitter_url" => $request->twitter_url,
+            ]);
+    
+            return redirect()->back();
+        }
+
+        
     }
 
     /**
