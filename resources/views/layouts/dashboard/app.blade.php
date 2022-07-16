b7bd6fe42c8169e6bf84d3ac5cb7b827ae23ac2d
islamdiab-stack <islamdiab967@gmail.com>
2022-07-16 06:47:55 +0300

    create a new modal to setting preview

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -367,6 +367,156 @@
         <!-- ./model-dialog -->
     </div>
 
+    <!-- Setting Profile Modal -->
+    @auth
+    <div class="modal setting-profile fade" id="profile-{{Auth::user()->id}}" data-bs-backdrop="static"
+        data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
+        aria-hidden="true">
+        <!-- model dialog -->
+        <div class="modal-dialog">
+            <!-- model dialog -->
+            <div class="modal-content">
+                <!-- model header -->
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">{{Auth::user()->name . "'s"}}</h5>
+                    <button type="button" class="btn-close"
+                        data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <!-- ./model-header -->
+
+                <!-- form -->
+                <form action="{{route("front.profile.update", ["id" => Auth::user()->id])}}"
+                    method="POST" enctype="multipart/form-data">
+
+                    <!-- model body -->
+                    <div class="modal-body">
+                        @csrf
+                        @method("PUT")
+
+                        <!-- image -->
+                        <div class="form-group mb-3">
+                            <div class="avatar-upload">
+                                <div class="avatar-edit">
+                                    <input type='file' id="imageUpload" name="image" accept=".png, .jpg, .jpeg" />
+                                    <label for="imageUpload">
+                                        <i class="fas fa-pen"></i>
+                                    </label>
+                                </div>
+                                <div class="avatar-preview">
+                                    <div id="imagePreview" style="background-image: url({{asset('uploads/users/' . Auth::user()->image)}});">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <!-- Name -->
+                                <div class="form-group mb-3">
+                                    <label class="form-label" for="name">Name</label>
+                                    <input class="form-control" type="text" name="name" id="name"
+                                        value="{{Auth::user()->name}}">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <!-- Email -->
+                                <div class="form-group mb-3">
+                                    <label class="form-label" for="email">Email</label>
+                                    <input class="form-control" type="email" name="email" id="email"
+                                        value="{{Auth::user()->email}}">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <!-- password -->
+                                <div class="form-group mb-3">
+                                    <label class="form-label" for="password">Password</label>
+                                    <input class="form-control" type="password" name="password" id="password"
+                                        required>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <!-- confirm password -->
+                                <div class="form-group mb-3">
+                                    <label class="form-label" for="password_confirm">Confirm Password</label>
+                                    <input class="form-control" type="password" name="password_confirmation"
+                                        id="password_confirm" required>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                        <!-- Linkedin -->
+                        <div class="input-group mb-3">
+                            <!-- Icon -->
+                            <span class="input-group-text" id="linkedin_url">
+                                <i class="fab fa-linkedin"></i>
+                            </span>
+                            <!-- Input -->
+                            <input class="form-control" type="text" aria-label="linkedin_url"
+                                aria-describedby="linkedin_url" name="linkedin_url"
+                                value="{{Auth::user()->linkedin_url}}">
+                        </div>
+
+                        <!-- Github -->
+                        <div class="input-group mb-3">
+                            <!-- Icon -->
+                            <span class="input-group-text" id="github_url">
+                                <i class="fab fa-github-square"></i>
+                            </span>
+                            <!-- Input -->
+                            <input class="form-control" type="text" aria-label="github_url"
+                                aria-describedby="github_url" name="github_url"
+                                value="{{Auth::user()->github_url}}">
+                        </div>
+
+                        <!-- facebook -->
+                        <div class="input-group mb-3">
+                            <!-- Icon -->
+                            <span class="input-group-text" id="facebook_url">
+                                <i class="fab fa-facebook-square"></i>
+                            </span>
+                            <!-- Input -->
+                            <input class="form-control" type="text" aria-label="facebook_url"
+                                aria-describedby="facebook_url" name="facebook_url"
+                                value="{{Auth::user()->facebook_url}}">
+                        </div>
+
+                        <!-- twitter -->
+                        <div class="input-group">
+                            <!-- Icon -->
+                            <span class="input-group-text" id="twitter_url">
+                                <i class="fab fa-twitter-square"></i>
+                            </span>
+                            <!-- Input -->
+                            <input class="form-control" type="text" aria-label="twitter_url"
+                                aria-describedby="twitter_url" name="twitter_url"
+                                value="{{Auth::user()->twitter_url}}">
+                        </div>
+
+                    </div>
+                    <!-- ./model-body -->
+
+                    <!-- model footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </div>
+                    <!-- ./model-footer -->
+
+                </form>
+                <!-- ./form -->
+
+            </div>
+                <!-- ./model-content -->
+        </div>
+        <!-- ./model-dialog -->
+    </div>
+    @endauth
 
     <!--   Core JS Files   -->
     <script src="{{asset("libs/jquery/js/jquery-3.5.1.js")}}"></script>
@@ -477,9 +627,25 @@
 
         $('.select2').select2();
 
-        // $(document).ready(function(){
+    </script>
 
-        // });
+    <script>
+        $(document).ready(function(){
+            function readURL(input) {
+                if (input.files && input.files[0]) {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+                        $('#imagePreview').hide();
+                        $('#imagePreview').fadeIn(650);
+                    }
+                    reader.readAsDataURL(input.files[0]);
+                }
+            }
+            $("#imageUpload").change(function() {
+                readURL(this);
+            });
+        });
     </script>
 
     {{-- Other scripts --}}
