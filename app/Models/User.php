4d7430469b85648d7310e7eeec10dcdd5f81f6f7
islamdiab-stack <islamdiab967@gmail.com>
2022-07-17 07:57:35 +0300

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,7 +66,7 @@ class User extends Authenticatable
      */
     public function articles()
     {
-        return $this->hasMany(User::class);
+        return $this->hasMany(Article::class);
     }
 
 }
