38b12a6820017728d828cfebd1f7eb811e95850d
ESSO <islamdiab967@gmail.com>
2022-10-28 01:27:18 +0200

    add a new column to slug

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
-    protected $fillable = ["name"];
+    protected $fillable = ["name", "slug"];
 
     /**
      * The article that belong to the Tag
