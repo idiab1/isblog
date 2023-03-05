a09d9eb4a2d5b9e7b809f25ef2f7c352b6c5a6b9
ESSO <islamdiab967@gmail.com>
2023-03-05 09:25:05 +0200

    create a new method to check on setting if less than 1 create default data

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,4 +9,20 @@ class Setting extends Model
 {
     use HasFactory;
     protected $fillable = ["name", "linkedin_url", "github_url"];
+
+    public static function checkSettings()
+    {
+        $settings = self::all();
+
+        if(count($settings) < 1)
+        {
+            self::create([
+                "name" => "IsBlog",
+                "linkedin_url" => "https://www.linkedin.com/in/islam-diab-47b98b176/",
+                "github_url" => "https://github.com/islamdiab-stack",
+            ]);
+        }
+
+        return self::first();
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        $settings = Setting::checkSettings();
+
+        view()->share([
+            "settings" => $settings
+        ]);
     }
 }
