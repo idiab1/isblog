8c6bd216424c9d12220762471a506b65718a63c3
ESSO <islamdiab967@gmail.com>
2023-03-05 10:56:33 +0200

    create a new request for users

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed'],
+            'facebook_url' => ['nullable'],
+            'github_url' => ['nullable'],
+            'linkedin_url' => ['nullable'],
+            'twitter_url' => ['nullable'],
+        ];
+    }
+}
